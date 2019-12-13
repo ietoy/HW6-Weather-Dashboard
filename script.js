f1d068ff4fc80ff7bc3b93771a93d735e893f8d8
Ian Toy <iet@ela.com>
2019-12-13 15:39:02 -0800

    added on click to diplay searched city, dynam gen to main-display

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ $(document).ready(function() {
 
 // These are our variables that look to elements on the dom
 var btnList = document.querySelector("#btn-list");
+var mainDislpayEl = document.querySelector("#main-display");
 
 // This is our array holding city names that the user has searched for
-var cityList = ["Denver", "Oakland"];
+var cityList = [];
 
 // This function adds buttons with the history of searched city names below the search bar
 $("#add-city").on("click", function() {
@@ -37,22 +38,100 @@ function renderBtns() {
 
 
   // This is the API query to OpenWeather
-  $(document).on("click", ".search-city", function() {
+  $(document).on("click", ".search-city", searchCity)
+
+
+  function searchCity() {
+
+    $("#main-display").empty();
+
     var currentCity = $(this).attr("data-city");
     var APIKey = "acc7c144d8d4d67c3bafe14ef897170e";
-    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&units=imperial&appid=" + APIKey;
-    console.log(queryURL);
-    // GETTING UNCAUGHT TYPE ERROR: $.AJAX IS NOT A FUNCTION HERE, PLZ HALP!
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&units=imperial&appid=" + APIKey;
+
     $.ajax({
     url: queryURL,
     method: "GET"
     }).then(function(response) {
 
-      // var results = response;
-      console.log("response");
-    });
+      var results = response;
 
-  });
+      var showCity = $("<div>");
+      showCity.attr("class", "card");
+      showCity.attr("class", "text-block");
+      showCity.attr("class", "bg-light");
+      showCity.attr("class", "mt-3");
+      showCity.attr("class", "mr-3");
+      $("#main-display").append(showCity);
+
+      var cityHeader = $("<div>");
+      cityHeader.attr("class", "card-header");
+      showCity.append(cityHeader);
+
+      var cityDisplayName = $("<h2>");
+      cityDisplayName.text(currentCity);
+      cityHeader.append(cityDisplayName);
+
+      var cityCardBody = $("<div>");
+      cityCardBody.attr("class", "card-body");
+      $("#main-display").append(cityCardBody);
+
+      var showTemp = $("<p>");
+      var cityTemp = results.main.temp;
+      showTemp.text("Temperature: " + cityTemp);
+      cityCardBody.append(showTemp);
+
+      var showHumidity = $("<p>");
+      var cityHumidity = results.main.humidity;
+      showHumidity.text("Humidity: " + cityHumidity);
+      cityCardBody.append(showHumidity);
+      
+      var showWindSpeed = $("<p>");
+      var cityWindSpeed = results.wind.speed;
+      showWindSpeed.text("Wind Speed: " + cityWindSpeed);
+      cityCardBody.append(showWindSpeed);
+       
+      // COME BACK TO UV INDEX
+      var showUVIndex = $("<p>");
+      // var cityUVIndex =;
+      
+      // FIVE DAY FORECAST 
+
+
+    });
+  };
+
+
+
+
+  // Main Display
+  // 
+  // <div class="col-10">
+  //   <div class="main-display">
+  //     <!-- START CARD -->
+  //     <div class="card text-block bg-light mt-3 mr-3">
+  //       <div class="card-header">
+  //         <h2>
+  //           City and Date Here
+  //         </h2>
+  //     </div>
+        
+  //     <div class="card-body">
+  //       <p class="card-text">Temp</p>
+  //       <br>
+  //       <p class="card-text">Humidity</p>
+  //       <br>
+  //       <p class="card-text">Wind Speed</p>
+  //       <br>
+  //       <p class="card-text">UV Index</p>
+  //     </div>
+  //     </div>
+  //   </div>
+
+
+
+
+
 
 // close doc ready
 });
