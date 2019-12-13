8e5d934d15a2a5c19de3244cdd4df4ced66284f9
Ian Toy <iet@ela.com>
2019-12-12 22:53:57 -0800

    cleaned up some css dislpay elements, added event listeners, started ajax query

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+$(document).ready(function() {
+
 // These are our variables that look to elements on the dom
 var btnList = document.querySelector("#btn-list");
 
 // This is our array holding city names that the user has searched for
 var cityList = [];
 
-// this function adds buttons with the history of searched city names below the search bar
+// This function adds buttons with the history of searched city names below the search bar
 $("#add-city").on("click", function() {
   event.preventDefault();
   var newCity;
@@ -13,6 +15,9 @@ $("#add-city").on("click", function() {
       cityList.push(newCity);
     };
   renderBtns();
+
+// ADD THE SEARCHCITY FUNCTION
+//   searchCity();
 });
 
 function renderBtns() {
@@ -23,83 +28,50 @@ function renderBtns() {
     newBtn.attr("type", "button");
     newBtn.attr("class", "btn");
     newBtn.attr("class", "btn-secondary")
+    newBtn.attr("class", "search-city")
+    newBtn.attr("data-city", cityList[i]);
     newBtn.text(btnText);
     $(btnList).append(newBtn);
   };
 };
 
 
+  // This is the API query to OpenWeather
+  $(document).on("click", ".search-city", function() {
+    var currentCity = $(this).attr("data-city");
+    var APIKey = "acc7c144d8d4d67c3bafe14ef897170e";
+    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&units=imperial&appid=" + APIKey;
+
+    // GETTING UNCAUGHT TYPE ERROR: $.AJAX IS NOT A FUNCTION HERE, PLZ HALP!
+    $.ajax({
+    url: queryURL,
+    method: "GET"
+    }).then(function(response) {
+
+      // var results = response;
+      console.log(response);
+    });
+  });
+});
+
+// Open Weather NAME & COUNTRY path
+// results.name
+// results.sys.country
+
+// Open Weather TEMP path
+// results.main.temp
+
+// Open Weather HUMIDITY path
+// results.main.humidity
+
+// Open Weather WIND SPEED
+// results.wind.speed
+
+// Open Weather UV INDEX
+// ?????????????????????
 
 
+// FIVE DAY FORECAST API
+// "api.openweathermap.org/data/2.5/forecast?q=" + currentCity + "&units=imperial&appid=" + APIKey
 
-// "api.openweathermap.org/data/2.5/weather?q=" + searchCity
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function renderBtns() {
-//     btnList.empty();
-//     for (var i = 0; i < cityList.length; i++) {
-//         var newBtn = $("<button>");
-//         newBtn.text(cityList[i]);
-//         btnList.append(newBtn);
-//     };
-// };
-
-// this is the function from the 16 all together activity that populates the animal buttons
-// function populateButtons(arrayToUse, classToAdd, areaToAddTo) {
-//     $(areaToAddTo).empty();
-//     for (var i = 0; i < arrayToUse.length; i++) {
-//         var newButton = $("<button>");
-//         newButton.addClass(classToAdd);
-//         newButton.attr("data-type", arrayToUse[i]);
-//         newButton.text(arrayToUse[i]);
-//         $(areaToAddTo).append(newButton);
-//       }
-//     }
-
-
-
-
-
-
-
-
-// addBtn function, currently non-functional >.<
-// function addBtn() {
-//   for (var i = 0; i < cityList.length; i++) {
-//     var newBtn = $("<button>");
-//     newBtn.text(cityList[i]);
-//     $("#button-list").append(newBtn);
-//   };  
-// };
-
-
-
-
-
-
-
-// $("#add-city").on("click", function() {
-//     console.log("add function works!")
-// });
+// api.openweathermap.org/data/2.5/forecast?q=london&units=imperial&appid=acc7c144d8d4d67c3bafe14ef897170e
