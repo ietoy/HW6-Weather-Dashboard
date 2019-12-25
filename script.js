543652a29c81908aebe66a2b06bf8e8e8b5fe529
Ian Toy <iet@ela.com>
2019-12-24 16:24:10 -0800

    updating latest changes before initializing new attempt

--- script.js
+++ script.js
@@ -40,12 +40,15 @@ function renderBtns() {
   // This is the API query to OpenWeather
   $(document).on("click", ".search-city", searchCity)
 
+  var currentCity;
 
   function searchCity() {
 
     $("#main-display").empty();
 
-    var currentCity = $(this).attr("data-city");
+    $("#five-day").empty();
+
+    currentCity = $(this).attr("data-city");
     var APIKey = "acc7c144d8d4d67c3bafe14ef897170e";
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&units=imperial&appid=" + APIKey;
 
@@ -92,68 +95,61 @@ function renderBtns() {
       cityCardBody.append(showWindSpeed);
        
       // COME BACK TO UV INDEX
-      var showUVIndex = $("<p>");
+      // var showUVIndex = $("<p>");
       // var cityUVIndex =;
       
-      // FIVE DAY FORECAST 
+      renderFiveDay();
 
-
+  
     });
   };
 
+      // FIVE DAY FORECAST 
+      
+      function renderFiveDay() {
+        var APIKey = "acc7c144d8d4d67c3bafe14ef897170e";
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + currentCity + "&units=imperial&appid=" + APIKey;
+  
+        $.ajax({
+        url: queryURL,
+        method: "GET"
+        }).then(function(response) {
 
+          var results = response;
 
+          for (var i = 0; i < results.length; i++) {
 
-  // Main Display
-  // 
-  // <div class="col-10">
-  //   <div class="main-display">
-  //     <!-- START CARD -->
-  //     <div class="card text-block bg-light mt-3 mr-3">
-  //       <div class="card-header">
-  //         <h2>
-  //           City and Date Here
-  //         </h2>
-  //     </div>
-        
-  //     <div class="card-body">
-  //       <p class="card-text">Temp</p>
-  //       <br>
-  //       <p class="card-text">Humidity</p>
-  //       <br>
-  //       <p class="card-text">Wind Speed</p>
-  //       <br>
-  //       <p class="card-text">UV Index</p>
-  //     </div>
-  //     </div>
-  //   </div>
+            var fiveDayCard = $("<div>");
+            fiveDayCard.attr("class", "card");
+            fiveDayCard.attr("class", "text-white");
+            fiveDayCard.attr("class", "bg-primary");
+            fiveDayCard.attr("class", "float-left");
+            fiveDayCard.attr("class", "mt-3");
+            fiveDayCard.attr("class", "mr-3");
+            $("#five-day").append(fiveDayCard);
+      
+            var fiveDayCardHeader = $("<div>");
+            fiveDayCardHeader.attr("class", "card-header");
+            fiveDayCard.append(fiveDayCardHeader);
+  
+            var fiveDayCardBody = $("<div>");
+            fiveDayCardBody.attr("class", "card-body");
+            fiveDayCard.append(fiveDayCardBody);
+      
+            var showFiveDayTemp = $("<p>");
+            // var fiveDayTemp = results[i].;
+            showFiveDayTemp.text(fiveDayTemp);
 
+            var showFiveDayHumidity = $("<p>");
+            var fiveDayHumidity = results[i].list.humidity;
+            showFiveDayHumidity.text(fiveDayHumidity);
 
+          };
+        });
 
+      };
 
 
 
 // close doc ready
 });
-
-// Open Weather NAME & COUNTRY path
-// results.name
-// results.sys.country
-
-// Open Weather TEMP path
-// results.main.temp
-
-// Open Weather HUMIDITY path
-// results.main.humidity
-
-// Open Weather WIND SPEED
-// results.wind.speed
-
-// Open Weather UV INDEX
-// ?????????????????????
-
-
-// FIVE DAY FORECAST API
-// "api.openweathermap.org/data/2.5/forecast?q=" + currentCity + "&units=imperial&appid=" + APIKey
-
-// api.openweathermap.org/data/2.5/forecast?q=london&units=imperial&appid=acc7c144d8d4d67c3bafe14ef897170e
